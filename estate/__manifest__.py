7302efe605b604040d2c5312076185d8595192c6
Florian Mataigne <flne@odoo.com>
2023-04-18 08:58:57 +0200

    [ADD] Chapter 1 - Module structure

---
+++ estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Real Estate",
+    "summary": """
+        Training module designed for partner training - Simulating Real Estate Custom Flow
+        """,
+    "category": "",
+    "version": "17.0.0.0.0",
+    "author": "Odoo PS",
+    "website": "https://www.odoo.com",
+    "license": "OEEL-1",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        # SECURITY
+        # VIEWS
+        # MENUS
+    ],
+}
