8952213ab60cc9e3ab796e3b10aa700dbe72a5e7
Florian Mataigne <flne@odoo.com>
2024-02-23 16:02:43 +0100

    [ADD] Chapter 6 - Basic Views

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,17 +1,21 @@
 from odoo import api, fields, models
-
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property Model"
 
+    def _default_date_availability(self):
+        return fields.Date.today() + relativedelta(months=3)
+
+    active = fields.Boolean(default=True)
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default=_default_date_availability, copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -24,4 +28,16 @@ class EstateProperty(models.Model):
             ("east", "East"),
             ("west", "West"),
         ],
+    )
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("received", "Offer Received"),
+            ("accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        default="new",
+        required=True,
+        copy=False,
     )
